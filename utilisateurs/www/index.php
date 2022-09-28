ee3a2043c3763a6bd096996e62dbad175fce6704
Mickaël <toto@crm.fr>
2022-09-28 12:24:37 +0200

    rappels bases php

--- utilisateurs/www/index.php
+++ utilisateurs/www/index.php
@@ -1,4 +1,31 @@
 <?php 
 
-echo '<p>Utilisateurs</p>';
+function direBonjour(): void
+{
+    echo "<h1>Bonjour</h1>";
+}
 
+function direBonjourAqui(string $nom): string
+{
+    return "<p>Bonjour $nom</p>";
+}
+
+direBonjour();
+direBonjour();
+
+echo direBonjourAqui('Toto');
+
+$bonjour = direBonjourAqui('Titi');
+echo $bonjour;
+
+$prenom  = "Bastian";
+
+echo direBonjourAqui($prenom);
+
+$nom = "Mike";
+
+echo "<p>Bonjour $nom</p>";
+
+echo '<p>Bonjour ' . $nom . '</p';
+
+require 'footer.php';
